d1765a34c046c1c917e25166e0e4d5d14056c455
TheMajorTechie <vincent.themajortechie@gmail.com>
2024-02-23 09:08:44 -0700

    Completed part 1 of lab 4

--- L4/Lab4/Core/Src/main.c
+++ L4/Lab4/Core/Src/main.c
@@ -66,17 +66,27 @@ static void MX_USB_PCD_Init(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
-void transmit_char(char CharacterToSend) {
+void transmit_char(char characterToSend) {
 	//check and wait on USART status flag indicating transmit register empty
 	
 	//do nothing until the TXE (transmit data bit) bit is set
 	while((USART3->ISR & USART_ISR_TXE) != USART_ISR_TXE) {
-		USART3->TDR = 'L';		//essentially a debug print statement
+		//USART3->TDR = 'L';		//essentially a debug print statement
 	}
 	
 	//write the character into the transmit data register
-	USART3->TDR = CharacterToSend;
+	USART3->TDR = characterToSend;
 }
+
+void transmit_string(char* stringToSend) {
+	while(*stringToSend != 0) {
+		transmit_char(*stringToSend);
+		stringToSend++;
+	}
+	
+	return;
+}
+
 /* USER CODE END 0 */
 
 /**
@@ -88,8 +98,11 @@ int main(void)
   HAL_Init();
   SystemClock_Config();
 
-	//enable GPIOB peripheral clock
+	//===================Configure system & USART
+	
+	//enable GPIOB & GPIOC peripheral clocks
 	RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
+	RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
 	
 	//configure USART
 	RCC->APB1ENR |= RCC_APB1ENR_USART3EN;		//enable system clock to USART3
@@ -100,23 +113,102 @@ int main(void)
 	
 	//set PB10 as TX and PB11 as RX for USART3
 	//this will use alternate function mode AF4
-	GPIOB->AFR[0] = 4;
+	GPIOB->AFR[1] |= ((1 << 10) | (1 << 14));
 	
 	//set baud rate to 115200 bits/second (oversampling by 16)
 	//can use HAL_RCC_GetHCLKFreq() to get system clock freq
 	USART3->BRR = (HAL_RCC_GetHCLKFreq() / 115200);
-	
+		
 	//enable USART transmit, receive, and the USART itself
 	USART3->CR1 |= ((USART_CR1_TE) | (USART_CR1_RE) | (USART_CR1_UE));
+	
+	//===================Configure LEDs
+	
+	//set the lower bits for PC6/7/8/9 in moder register for general-purpose output mode
+	GPIOC->MODER |= ((1 << 12) | (1 << 14) | (1 << 16) | (1 << 18));
+	
+	//clear the bits for output type to put into push-pull
+	GPIOC->OTYPER &= ~((1 << 6) | (1 << 7) | (1 << 8) | (1 << 9));
+	
+	//clear the lower bits for the output speed register to put into low speed
+	GPIOC->OSPEEDR &= ~((1 << 12) | (1 << 14) | (1 << 16) | (1 << 18));
+	
+	//clear both upper and lower bits to set no pull-ups or pull-downs
+	GPIOC->PUPDR &= ~((1 << 12) | (1 << 13) | 
+										(1 << 14) | (1 << 15) | 
+										(1 << 16) | (1 << 17) |
+										(1 << 18) | (1 << 19)
+										);
+										
+	//start all LEDs initially turned off
+	GPIOC->ODR &= ~(1 << 6);	//red
+	GPIOC->ODR &= ~(1 << 7);	//blue
+	GPIOC->ODR &= ~(1 << 8);	//orange
+	GPIOC->ODR &= ~(1 << 9);	//green
+	
+	//======================Misc	
+	
+	//A variable to store what character has been sent
+	char receivedChar;
+	
+	
 	
 	
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    /* USER CODE END WHILE */
-		transmit_char('@');
-		HAL_Delay(500);
-    /* USER CODE BEGIN 3 */
+		//transmit_char('@');
+		//transmit_string("This is a test string\n");
+		//HAL_Delay(500);
+		
+		//Check if the receive register has something in it
+		while((USART3->ISR & USART_ISR_RXNE) == USART_ISR_RXNE) {
+			receivedChar = (USART3->RDR);
+			transmit_char(receivedChar);
+		}
+		
+		switch(receivedChar) {
+				case 'r': {
+					USART3->TDR = 10;
+					USART3->TDR = 13;
+					transmit_string("Toggled color: Red. Please input your next color: ");
+					GPIOC->ODR ^= (1 << 6);
+					while((USART3->ISR & USART_ISR_RXNE) != USART_ISR_RXNE) { }		//prevent the text from being sent like crazy
+					break;
+				}
+				case 'b': {
+					USART3->TDR = 10;
+					USART3->TDR = 13;
+					transmit_string("Toggled color: Blue. Please input your next color: ");
+					GPIOC->ODR ^= (1 << 7);
+					while((USART3->ISR & USART_ISR_RXNE) != USART_ISR_RXNE) { }		//prevent the text from being sent like crazy
+					break;
+				}
+				case 'o': {
+					USART3->TDR = 10;
+					USART3->TDR = 13;
+					transmit_string("Toggled color: Orange. Please input your next color: ");
+					GPIOC->ODR ^= (1 << 8);
+					while((USART3->ISR & USART_ISR_RXNE) != USART_ISR_RXNE) { }		//prevent the text from being sent like crazy
+					break;
+				}
+				case 'g': {
+					USART3->TDR = 10;
+					USART3->TDR = 13;
+					transmit_string("Toggled color: Green. Please input your next color: ");
+					GPIOC->ODR ^= (1 << 9);
+					while((USART3->ISR & USART_ISR_RXNE) != USART_ISR_RXNE) { }		//prevent the text from being sent like crazy
+					break;
+				}
+				default: {
+					transmit_string("\nThat is not a valid color character! Select from: \"r\", \"b\", \"o\", \"g\": ");
+					while((USART3->ISR & USART_ISR_RXNE) != USART_ISR_RXNE) { }		//prevent the text from being sent like crazy
+					break;
+				}
+		}
+		
+		
+		
   }
   /* USER CODE END 3 */
 }
